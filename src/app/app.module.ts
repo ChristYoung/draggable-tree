d4b835c2831514a5ecd4762ec90635cb7238d148
yangjie <jie.yang@witontek.com>
2023-03-18 10:21:35 +0800

    🐳 chore: add home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TreeListComponent } from './components/tree-list/tree-list.component';
+import { DraggableDirective } from './directives/draggable.directive';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TreeListComponent
+    TreeListComponent,
+    HomeComponent,
+    DraggableDirective,
   ],
   imports: [
     BrowserModule,
