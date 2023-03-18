87d5e9d04a740f8e6330bf47eca66702ee53a002
yangjie <jie.yang@witontek.com>
2023-03-18 10:50:32 +0800

    🐳 chore: home structure

--- src/app/zorro/zorro.module.ts
+++ src/app/zorro/zorro.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 @NgModule({
   declarations: [],
   exports: [
-    NzButtonModule, NzCheckboxModule, NzIconModule
+    NzButtonModule, NzCheckboxModule, NzIconModule,
+    NzGridModule,
   ]
 })
 export class ZorroModule { }
