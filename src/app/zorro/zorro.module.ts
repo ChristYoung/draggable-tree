28607c025a7dbac12b8e1845b21375e0339ca942
yangjie <jie.yang@witontek.com>
2023-03-18 17:31:54 +0800

    🌈 style: modify

--- src/app/zorro/zorro.module.ts
+++ src/app/zorro/zorro.module.ts
@@ -4,13 +4,14 @@ import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
 // import { CdkTreeModule } from '@angular/cdk/tree';
 
 @NgModule({
   declarations: [],
   exports: [
     NzButtonModule, NzCheckboxModule, NzIconModule,
-    NzGridModule, NzSpinModule,
+    NzGridModule, NzSpinModule, NzSkeletonModule,
     // CdkTreeModule
   ]
 })
