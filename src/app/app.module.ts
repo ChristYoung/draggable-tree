d8f0ba716f65f508fb1801d3add0b491b744fcad
yangjie <jie.yang@witontek.com>
2023-03-18 10:37:15 +0800

    🌈 style: use Ant Design Style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,15 @@ import { AppComponent } from './app.component';
 import { TreeListComponent } from './components/tree-list/tree-list.component';
 import { DraggableDirective } from './directives/draggable.directive';
 import { HomeComponent } from './pages/home/home.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_CN } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
@@ -16,9 +25,14 @@ import { HomeComponent } from './pages/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: zh_CN }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
